f30fe863a7b0562a2b4edbdce0a3b850b3b5bb33
Sahil Sahu <sahilsahu731709@gmail.com>
2025-11-12 19:08:35 +0530

    add image dialog with input

--- client/src/components/ImageDialog.tsx
+++ client/src/components/ImageDialog.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from './ui/dialog'
 import { Button } from './ui/button'
+import { useAuthStore } from '@/lib/store'
+import Image from 'next/image'
+import { User } from 'lucide-react'
 
 const ImageDialog = ({isIamgeDialogOpen, setIsIamgeDialogOpen} : {isIamgeDialogOpen: boolean, setIsIamgeDialogOpen: React.Dispatch<React.SetStateAction<boolean>>}) => {
+
+  const {user} = useAuthStore((state) => state)
+  const [image, setImage] = useState<string | null>(null)
+
+  const changeProfilehandler = async () =>{
+    if(!image) return;
+    console.log(image)
+  }
+
   return (
-    <div>
-      <Dialog open={isIamgeDialogOpen}>
-        <DialogContent>
+    <div className='z-50 '>
+      <Dialog open={isIamgeDialogOpen} onOpenChange={() => setIsIamgeDialogOpen(false)}>
+        <DialogContent className='bg-gray-600 '>
           <DialogHeader>
-            <DialogTitle>Change Profile Picture</DialogTitle>
+            <DialogTitle className='text-white text-xl text-center'>Change Profile Picture</DialogTitle>
           </DialogHeader>
+          <div className='flex items-center justify-center mt-10'>
+             {image ? <Image src={image || '/avatar.svg'} alt='profile' width={200} height={200} className='rounded-full' />
+             : <div className='w-50 h-50 rounded-full bg-gray-500 flex items-center justify-center'>
+                <User className='w-24 h-24 text-white' />
+              </div>
+             }
+          </div>
+          <div className='flex items-center justify-center'>
+            <input type='file' className='hidden' id='image' onChange={(e) => setImage(URL.createObjectURL(e.target.files![0]))} />
+            <label htmlFor='image' className='cursor-pointer text-white text-lg p-2 border rounded-xl border-white'>Choose Image</label>
+          </div>
           <DialogFooter>
             <Button onClick={() => setIsIamgeDialogOpen(false)}>Cancel</Button>
-            <Button onClick={() => setIsIamgeDialogOpen(false)}>Upload</Button>
+            <Button onClick={changeProfilehandler}>Upload</Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
